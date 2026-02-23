1e20f47e9d5753550a2fab56039815ec13fada40
vitorgalluci <vitorgallucibatista@gmail.com>
2026-02-23 08:47:34 -0300

    Add files via upload

---
+++ js/variaveis.js
@@ -0,0 +1,27 @@
+var nomeDaVariavel = "valor da variavel";
+var outraVariavel = "valor da outra variavel";
+var VariavelIdade = 22;
+var VariavelTenhoCabeloBranco = false;
+var VariavelIndefinida ;
+
+console.log(nomeDaVariavel);
+console.log(outraVariavel);
+console.log(VariavelIdade);
+console.log(VariavelTenhoCabeloBranco);
+console.log(VariavelIndefinida);
+ 
+
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+//Neste caso tr irá conter o valor true e fl o valor false.
+var int1 = tr + 1;
+var int2 = fl + 1;
+// A variável int1 irá conter o valor 2 (true + 1)
+console.log(a);
+console.log(b);
+console.log(tr);
+console.log(fl);
+console.log(int1);
+console.log(int2);
